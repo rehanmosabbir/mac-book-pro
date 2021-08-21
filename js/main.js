410391a416d6dcbb8e30bfa1831b45c8bad7592b
Mosabbir Rehan <rehanmosabbir@gmail.com>
2021-08-21 20:35:24 +0600

    add more features

--- js/main.js
+++ js/main.js
@@ -22,6 +22,55 @@ document.getElementById("sixteen-gb-memory-btn").addEventListener("click", funct
   totalPriceFinalEl.innerText = totalPrice;
 });
 
+document.getElementById("eight-gb-memory-btn").addEventListener("click", function () {
+  let extraMemoryCostEl = document.getElementById("extra-memory-cost");
+  extraMemoryCostEl.innerText = 0;
+  const extraMemoryCostText = extraMemoryCostEl.innerText;
+  const extraMemoryCostVal = parseInt(extraMemoryCostText);
+  extraMemoryCostEl.innerText = extraMemoryCostVal;
+
+  const extraStorageCostEl = document.getElementById("extra-storage-cost");
+  const extraStorageCostText = extraStorageCostEl.innerText;
+  const extraStorageCostVal = parseInt(extraStorageCostText);
+
+  const deliveryCostEl = document.getElementById("delivery-charge");
+  const deliveryCostText = deliveryCostEl.innerText;
+  const deliveryCostVal = parseInt(deliveryCostText);
+
+  const bestPriceEl = document.getElementById("best-price");
+  const bestPriceText = bestPriceEl.innerText;
+  const bestPriceVal = parseInt(bestPriceText);
+  const totalPrice = extraMemoryCostVal + bestPriceVal + extraStorageCostVal + deliveryCostVal;
+  const totalPriceEl = document.getElementById("total-price");
+  totalPriceEl.innerText = totalPrice;
+  const totalPriceFinalEl = document.getElementById("total-price-final");
+  totalPriceFinalEl.innerText = totalPrice;
+});
+
+document.getElementById("two-hundred-fifty-six-storage-btn").addEventListener("click", function () {
+  const extraMemoryCostEl = document.getElementById("extra-memory-cost");
+  const extraMemoryCostText = extraMemoryCostEl.innerText;
+  const extraMemoryCostVal = parseInt(extraMemoryCostText);
+
+  const deliveryCostEl = document.getElementById("delivery-charge");
+  const deliveryCostText = deliveryCostEl.innerText;
+  const deliveryCostVal = parseInt(deliveryCostText);
+
+  let extraStorageCostEl = document.getElementById("extra-storage-cost");
+  extraStorageCostEl.innerText = 0;
+  const extraStorageCostText = extraStorageCostEl.innerText;
+  const extraStorageCostVal = parseInt(extraStorageCostText);
+  extraStorageCostEl.innerText = extraStorageCostVal;
+
+  const bestPriceEl = document.getElementById("best-price");
+  const bestPriceText = bestPriceEl.innerText;
+  const bestPriceVal = parseInt(bestPriceText);
+  const totalPrice = extraStorageCostVal + bestPriceVal + extraMemoryCostVal + deliveryCostVal;
+  const totalPriceEl = document.getElementById("total-price");
+  totalPriceEl.innerText = totalPrice;
+  const totalPriceFinalEl = document.getElementById("total-price-final");
+  totalPriceFinalEl.innerText = totalPrice;
+});
 document.getElementById("five-hundred-twelve-storage-btn").addEventListener("click", function () {
   const extraMemoryCostEl = document.getElementById("extra-memory-cost");
   const extraMemoryCostText = extraMemoryCostEl.innerText;
@@ -69,6 +118,30 @@ document.getElementById("one-tb-storage-btn").addEventListener("click", function
   totalPriceFinalEl.innerText = totalPrice;
 });
 
+document.getElementById("free-delivery-charge-btn").addEventListener("click", function () {
+  let deliveryCostEl = document.getElementById("delivery-charge");
+  deliveryCostEl.innerText = 0;
+  const deliveryCostText = deliveryCostEl.innerText;
+  const deliveryCostVal = parseInt(deliveryCostText);
+  deliveryCostEl.innerText = deliveryCostVal;
+
+  const extraStorageCostEl = document.getElementById("extra-storage-cost");
+  const extraStorageCostText = extraStorageCostEl.innerText;
+  const extraStorageCostVal = parseInt(extraStorageCostText);
+
+  const extraMemoryCostEl = document.getElementById("extra-memory-cost");
+  const extraMemoryCostText = extraMemoryCostEl.innerText;
+  const extraMemoryCostVal = parseInt(extraMemoryCostText);
+
+  const bestPriceEl = document.getElementById("best-price");
+  const bestPriceText = bestPriceEl.innerText;
+  const bestPriceVal = parseInt(bestPriceText);
+  const totalPrice = extraMemoryCostVal + bestPriceVal + extraStorageCostVal + deliveryCostVal;
+  const totalPriceEl = document.getElementById("total-price");
+  totalPriceEl.innerText = totalPrice;
+  const totalPriceFinalEl = document.getElementById("total-price-final");
+  totalPriceFinalEl.innerText = totalPrice;
+});
 document.getElementById("delivery-charge-btn").addEventListener("click", function () {
   const deliveryCostEl = document.getElementById("delivery-charge");
   const deliveryCostText = deliveryCostEl.innerText;
